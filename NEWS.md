7e3c0fab3730d3324bb61e3b2f8a1a7ac68760f7
Michel Lang <michellang@gmail.com>
2017-06-14 21:49:05 +0000

    version 1.2

--- NEWS.md
+++ NEWS.md
@@ -1,7 +1,13 @@
+# base64url 1.2
+
+* Native routines are now registered.
+* Vignette builds without `microbenchmark` installed.
+* Update to new backports.
+
 # base64url 1.1
 
 * Added `base32_encode()` and `base32_decode()`.
-* Changes license to GPL-3.
+* Changed license to GPL-3.
 * Fixed vignette title.
 
 # base64url 1.0
--- inst/doc/Benchmarks.R
+++ inst/doc/Benchmarks.R
@@ -1,4 +1,7 @@
-## ------------------------------------------------------------------------
+## ----include=FALSE-------------------------------------------------------
+do.eval = requireNamespace("microbenchmark", quietly = TRUE)
+
+## ---- eval=do.eval-------------------------------------------------------
 library(base64url)
 library(base64enc)
 library(openssl)
@@ -11,7 +14,7 @@ microbenchmark(
   openssl = base64_encode(x)
 )
 
-## ------------------------------------------------------------------------
+## ---- eval = do.eval-----------------------------------------------------
 x = "N0JBLlRaUTp1bi5KOW4xWStNWEJoLHRQaDZ3"
 microbenchmark(
   base64url = base64_urldecode(x),
@@ -19,7 +22,7 @@ microbenchmark(
   openssl = rawToChar(base64_decode(x))
 )
 
-## ------------------------------------------------------------------------
+## ---- eval = do.eval-----------------------------------------------------
 rand = function(n, min = 1, max = 32) {
   chars = c(letters, LETTERS, as.character(0:9), c(".", ":", ",", "+", "-", "*", "/"))
   replicate(n, paste0(sample(chars, sample(min:max, 1), replace = TRUE), collapse = ""))
@@ -27,7 +30,7 @@ rand = function(n, min = 1, max = 32) {
 set.seed(1)
 rand(10)
 
-## ------------------------------------------------------------------------
+## ---- eval = do.eval-----------------------------------------------------
 base64enc_encode = function(x) {
   vapply(x, function(x) base64encode(charToRaw(x)), NA_character_, USE.NAMES = FALSE)
 }
@@ -44,7 +47,7 @@ openssl_decode = function(x) {
   vapply(x, function(x) rawToChar(base64_decode(x)), NA_character_, USE.NAMES = FALSE)
 }
 
-## ------------------------------------------------------------------------
+## ---- eval = do.eval-----------------------------------------------------
 set.seed(1)
 x = rand(1000)
 microbenchmark(
